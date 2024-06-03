7619468360e442681b2d05178ba532931665d51e
Jordan Millett <JelloJordan3@gmail.com>
2024-06-02 22:49:35 -0700

    Update

--- generate_songlist.bat
+++ generate_songlist.bat
@@ -3,15 +3,20 @@ chcp 65001 > nul
 setlocal enabledelayedexpansion
 
 set "songDirectory=wwwroot\data\songs"
-set "outputFile=wwwroot\data\songlist.txt"
+set "outputFile=wwwroot\data\songlinks.txt"
+set "songNamesFile=wwwroot\data\songnames.txt"
 
-REM Remove existing songlist.txt file
+REM Remove existing files
 if exist "%outputFile%" del "%outputFile%"
+if exist "%songNamesFile%" del "%songNamesFile%"
 
-REM Iterate through files in song directory and write their names to songlist.txt
+REM Iterate through files in song directory, write their names to songlinks.txt
+REM and first lines to songnames.txt
 for %%f in ("%songDirectory%\*.*") do (
     set "fileName=%%~nxf"
     echo !fileName! >> "%outputFile%"
+    set /p firstLine=<"%%f"
+    echo !firstLine! >> "%songNamesFile%"
 )
 
-echo Song list generated successfully.
+echo Song links and names generated successfully.
